12456d10d9858951b2f8669b7bbb0762843e57df
daniele.minatto <daniele.minatto@mimo.com.br>
2023-08-23 09:53:43 -0300

    feat: change class name

---
+++ min-rotated-array/MinimumRotatedSortedArray/MinimumRotatedSortedArray/MinimumRotatedSortedArray.cs
@@ -0,0 +1,25 @@
+﻿namespace MinimumRotatedSortedArray;
+
+public class MinimumRotatedSortedArray
+{
+    public int FindMin(int[] numbers) {
+
+        int start = 0;
+        int end = numbers.Length - 1;
+
+        if (numbers[start] <= numbers[end]) return numbers[start];
+
+        while(start < end) {
+            int mid = (start + (end - start)/2);
+
+            if(numbers[mid] >= numbers[0]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return numbers[start];        
+    }
+
+}
